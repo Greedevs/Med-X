2ec067292ed8c849576bf119dea725443b86a2b8
abdurrohmanq <qodirovabdurahmon46@gmail.com>
2023-09-21 14:50:16 +0500

    Added DoctorService and DoctorsController

---
+++ src/MedX.WebApi/Controllers/DoctorsController.cs
@@ -0,0 +1,22 @@
+﻿using MedX.Service.DTOs.Doctors;
+using MedX.Service.Interfaces;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MedX.WebApi.Controllers;
+
+public class DoctorsController : BaseController
+{
+    private readonly IDoctorService doctorService;
+    public DoctorsController(IDoctorService doctorService)
+    {
+        this.doctorService = doctorService;
+    }
+
+    [HttpPost]
+
+    public Task<IActionResult> PostAsync(DoctorCreationDto dto)
+    {
+        return Ok()
+    }
+}
