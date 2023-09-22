f51adde33fe157cb10ae9720b2ac3299fb90cf56
Avazbek0704 <siddikovavazbek213@gmail.com>
2023-09-22 09:32:38 +0500

    Middleware and selog added

---
+++ src/MedX.WebApi/Middlewares/ExceptionHandlerMiddleware.cs
@@ -0,0 +1,62 @@
+﻿using MedX.Service.Exceptions;
+using MedX.WebApi.Models;
+
+namespace MedX.WebApi.Middlewares;
+
+public class ExceptionHandlerMiddleware
+{
+    public readonly RequestDelegate request;
+    public readonly ILogger<ExceptionHandlerMiddleware> logger;
+    public ExceptionHandlerMiddleware(RequestDelegate request, ILogger<ExceptionHandlerMiddleware> logger)
+    {
+        this.request = request;
+        this.logger = logger;
+    }
+
+    public async Task Invoke(HttpContext context)
+    {
+        try
+        {
+            await this.request(context);
+        }
+        catch (NotFoundException ex)
+        {
+            context.Response.StatusCode = ex.StatusCode;
+            await context.Response.WriteAsJsonAsync(new Response
+            {
+                StatusCode = context.Response.StatusCode,
+                Message = ex.Message,
+            });
+        }
+        catch (AlreadyExistException ex)
+        {
+            context.Response.StatusCode = ex.StatusCode;
+            await context.Response.WriteAsJsonAsync(new Response
+            {
+                StatusCode = context.Response.StatusCode,
+                Message = ex.Message,
+            });
+        }
+        catch (CustomException ex)
+        {
+            context.Response.StatusCode = ex.StatusCode;
+            await context.Response.WriteAsJsonAsync(new Response
+            {
+                StatusCode = context.Response.StatusCode,
+                Message = ex.Message,
+            });
+        }
+        catch (Exception ex)
+        {
+            context.Response.StatusCode = 500;
+            this.logger.LogError(ex.ToString());
+            await context.Response.WriteAsJsonAsync(new Response
+            {
+                StatusCode = context.Response.StatusCode,
+                Message = ex.Message,
+            });
+            logger.LogError(ex.ToString());
+        }
+    }
+
+}
