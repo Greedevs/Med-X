8a478f42bbd784d3c1b36967dfe0545fd6c45cc5
abdurrohmanq <qodirovabdurahmon46@gmail.com>
2023-09-22 18:19:08 +0500

    PatientService errors fixed

--- src/MedX.Service/Services/PatientService.cs
+++ src/MedX.Service/Services/PatientService.cs
@@ -7,6 +7,7 @@ using MedX.Domain.Configurations;
 using MedX.Service.Exceptions;
 using MedX.Service.Extensions;
 using MedX.Service.DTOs.Doctors;
+using Microsoft.EntityFrameworkCore;
 
 namespace MedX.Service.Services;
 
@@ -48,25 +49,11 @@ public class PatientService : IPatientService
         return true;
     }
 
-    public async Task<IEnumerable<PatientResultDto>> GetAllAsync(PaginationParams @params, string search = null)
-    {
-        var patients=this.repository.GetAll(includes: new[] { "Treatments", "Transactions", "Appointments" })
-            .ToPaginate(@params)
-            .ToList();
-
-        if (search is not null)
-        {
-            patients = patients.Where(user => user.LastName.Contains(search, StringComparison.OrdinalIgnoreCase)).ToList();
-        }
-
-        return this.mapper.Map<IEnumerable<PatientResultDto>>(patients);
-
-    }
 
     public async Task<PatientResultDto> GetAsync(long id)
     {
-        Patient existPatient = this.repository.GetAll(includes: new[] {"Treatments","Transactions","Appointments"}).FirstOrDefault(p=>p.Id.Equals(id))
-            ?? throw new AlreadyExistException($"This patient is not found {id}");
+        Patient existPatient = await this.repository.GetAsync(p => p.Id == id)
+            ?? throw new NotFoundException($"This patient is not found {id}");
         
         PatientResultDto result=this.mapper.Map<PatientResultDto>(existPatient);
         return result;
@@ -74,10 +61,10 @@ public class PatientService : IPatientService
 
     public async Task<IEnumerable<PatientResultDto>> SearchByQuery(string query)
     {
-        var result = this.repository.GetAll(includes: new[] { "Treatments", "Transactions", "Appointments" })
+        var result = await this.repository.GetAll(includes: new[] { "Treatments", "Transactions", "Appointments" })
         .Where(d => d.Pinfl.Contains(query) ||
         d.FirstName.Contains(query) || d.LastName.Contains(query) ||
-        d.SurName.Contains(query) || d.Phone.Contains(query) || d.Address.Contains(query)).ToList();
+        d.SurName.Contains(query) || d.Phone.Contains(query) || d.Address.Contains(query)).ToListAsync();
 
         if (result is not null)
             return this.mapper.Map<IEnumerable<PatientResultDto>>(result);
@@ -86,15 +73,29 @@ public class PatientService : IPatientService
 
     public async Task<PatientResultDto> UpdateAsync(PatientUpdateDto dto)
     {
-        Patient patient = this.repository.GetAll().FirstOrDefault(p => p.Id.Equals(dto.Id))
+        Patient existPatient = await this.repository.GetAsync(p => p.Id == dto.Id)
             ?? throw new NotFoundException($"This patient is not found {dto.Id}");
 
-        this.mapper.Map(dto, patient);
-        patient.UpdatedAt = DateTime.UtcNow;
-        this.repository.Update(patient);
+        this.mapper.Map(dto, existPatient);
+        this.repository.Update(existPatient);
         await this.repository.SaveChanges();
 
-        PatientResultDto result = this.mapper.Map<PatientResultDto>(patient);
+        PatientResultDto result = this.mapper.Map<PatientResultDto>(existPatient);
         return result;
+    }
+
+    public async Task<IEnumerable<PatientResultDto>> GetAllAsync(PaginationParams @params, string search = null)
+    {
+        var patients= await this.repository.GetAll(includes: new[] { "Treatments", "Transactions", "Appointments" })
+            .ToPaginate(@params)
+            .ToListAsync();
+
+        if (search is not null)
+        {
+            patients = patients.Where(user => user.LastName.Contains(search, StringComparison.OrdinalIgnoreCase)).ToList();
+        }
+
+        return this.mapper.Map<IEnumerable<PatientResultDto>>(patients);
+
     }
 }
