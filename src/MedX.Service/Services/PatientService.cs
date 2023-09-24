8d75138ead5a86a8dac5231aff4a544495a0a142
abdurrohmanq <qodirovabdurahmon46@gmail.com>
2023-09-24 10:16:18 +0500

    PatientService Some fixed

--- src/MedX.Service/Services/PatientService.cs
+++ src/MedX.Service/Services/PatientService.cs
@@ -40,7 +40,7 @@ public class PatientService : IPatientService
     public async Task<bool> DeleteAsync(long id)
     {
         Patient patient = this.repository.GetAll().FirstOrDefault(p => p.Id.Equals(id));
-        if (patient is not null && patient.IsDeleted == true)
+        if(patient is not null)
             throw new NotFoundException($"This patient is not found {id}");
 
         this.repository.Delete(patient);
@@ -95,6 +95,5 @@ public class PatientService : IPatientService
         }
 
         return this.mapper.Map<IEnumerable<PatientResultDto>>(patients);
-
     }
 }
