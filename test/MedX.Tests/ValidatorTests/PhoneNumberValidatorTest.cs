8bb9eb994e43ce12aa906de7b1b8e53522785e09
Muqimjon <muqimjon.mr@gmail.com>
2023-10-29 17:59:12 +0500

    FIlled to examples PhoneValidationTest

--- test/MedX.Tests/ValidatorTests/PhoneNumberValidatorTest.cs
+++ test/MedX.Tests/ValidatorTests/PhoneNumberValidatorTest.cs
@@ -1,11 +1,19 @@
-﻿namespace MedX.Tests.ValidatorTests;
+﻿using MedX.Service.Validators;
+
+namespace MedX.Tests.ValidatorTests;
 
 public class PhoneNumberValidatorTest
 {
     [Theory]
     [InlineData(null)]
-    public void valid(string phoneNumber)
+    [InlineData("+99937349808")]
+    [InlineData("+9989373498O8")]
+    [InlineData("+998927349808")]
+    [InlineData("998937349808")]
+    public void ShoulReturnFalse(string phoneNumber)
     {
-
+        var validator = new PhoneNumberValidator();
+        var result = validator.IsValid(phoneNumber);
+        Assert.False(result);
     }
 }
